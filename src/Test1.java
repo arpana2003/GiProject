edd5b4923ce946fc0b2ad4fbf5d981139935b727
User <User@DESKTOP-EFL1N75>
2023-04-03 15:47:38 +0530

    Ist Commit

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+public class Test1 {
+
+	public static void main(String ar[])
+	{
+		String s1="Yes";
+		
+		if(s1.equals("Yes"))
+		{
+			System.out.println("Do Something...");
+		
+		}
+		else
+		{
+			System.out.println("Do Nothing");
+		}
+	}
+	
+}
